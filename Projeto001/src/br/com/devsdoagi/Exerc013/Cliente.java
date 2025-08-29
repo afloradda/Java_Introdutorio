7949dafc9111368d86775ce5d608793c0c810037
4ndrewsec5 <4ndrewsec5@gmail.com>
2025-08-29 09:25:45 -0300

    Feat: Adicionando classe Cliente e main

--- Projeto001/src/br/com/devsdoagi/Exerc013/Cliente.java
+++ Projeto001/src/br/com/devsdoagi/Exerc013/Cliente.java
@@ -1,4 +1,35 @@
 package br.com.devsdoagi.Exerc013;
 
+import java.util.Objects;
+
 public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (CPF: " + cpf + ")";
+    }
 }
